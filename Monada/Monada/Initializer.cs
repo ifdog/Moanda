b17aca4523d8539632f8f85eab1e1174f72ffa5b
ifdog <ifdog@outlook.com>
2017-04-29 21:44:07 +0800

    Base structure

---
+++ Monada/Monada/Initializer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using Caliburn.Micro;
+using Monada.ViewModels;
+
+namespace Monada
+{
+    public class Initializer:BootstrapperBase
+    {
+        private SimpleContainer _container;
+        public Initializer()
+        {
+            Initialize();
+        }
+        protected override void Configure()
+        {
+            _container = new SimpleContainer();
+            _container.Instance(_container);
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>()
+                .Singleton<MainViewModel>();
+            
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+        protected override object GetInstance(Type serviceType, string key)
+        {
+            return _container.GetInstance(serviceType, key);
+        }
+        protected override IEnumerable<object> GetAllInstances(Type serviceType)
+        {
+            return _container.GetAllInstances(serviceType);
+        }
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+        protected override void OnUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            MessageBox.Show(e.Exception.Message, "An error as occurred", MessageBoxButton.OK);
+        }
+    }
+}
