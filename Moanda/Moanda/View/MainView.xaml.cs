757aad28051379d3b860eeff5fe0c1ebf3befda6
ifdog <ifdog@outlook.com>
2017-05-04 21:15:15 +0800

    Update

--- Moanda/Moanda/View/MainView.xaml.cs
+++ Moanda/Moanda/View/MainView.xaml.cs
@@ -10,19 +10,22 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Moanda.ViewModel;
 
 namespace Moanda.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainView : Window
     {
-        public MainWindow()
+        public MainViewModel ViewModel { get; private set; }
+        public MainView()
         {
+            ViewModel = new MainViewModel();
             InitializeComponent();
+            DataContext = ViewModel;
         }
     }
 }
---
+++ Moanda/Moanda/ViewModel/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace Moanda.ViewModel
+{
+    public class MainViewModel:ReactiveObject
+    {
+        private string _mainIconPath = @"\Payload\Icon\circular_menu.ico";
+        public string MainIconPath
+        {
+            get => _mainIconPath;
+            set => this.RaiseAndSetIfChanged(ref _mainIconPath, value);
+        }
+    }
+}
